1085d565d25685db964f2c4cec52dfeda84befe4
Hamza Khan <hamzasafdark@gmail.com>
2024-11-18 18:18:36 +0500

    open device gallery for photo selection

--- app/src/main/java/com/squarekernels/pairperfect/CreateActivity.kt
+++ app/src/main/java/com/squarekernels/pairperfect/CreateActivity.kt
@@ -2,12 +2,14 @@ package com.squarekernels.pairperfect
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
@@ -21,9 +23,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squarekernels.pairperfect.models.BoardSize
 import com.squarekernels.pairperfect.utils.EXTRA_BOARD_SIZE
+import com.squarekernels.pairperfect.utils.isPermissionGranted
+import com.squarekernels.pairperfect.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
 
+    companion object {
+        private const val PICK_PHOTO_CODE  = 665
+        private const val READ_EXTERNAL_PHOTOS_CODE = 248
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
     private lateinit var rvImagePicker: RecyclerView
     private lateinit var etGameName: EditText
     private lateinit var btnSave: Button
@@ -61,7 +70,12 @@ class CreateActivity : AppCompatActivity() {
 
         rvImagePicker.adapter = ImagePickerAdapter(this, chosenImagesUris, boardSize, object: ImagePickerAdapter.ImageClickListener {
             override fun onPlaceHolderClicked() {
-                launchIntentForPhotos()
+                if (isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION )) {
+                    launchIntentForPhotos()
+                } else {
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTOS_CODE)
+                }
+
             }
         })
         rvImagePicker.setHasFixedSize(true)
@@ -69,11 +83,27 @@ class CreateActivity : AppCompatActivity() {
     }
 
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        if (requestCode == READ_EXTERNAL_PHOTOS_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else {
+                Toast.makeText(this, "In order to create a custom game, the game needs permission to access photos", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+    }
+
     private fun launchIntentForPhotos() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-        startActivityForResult(Intent.createChooser(intent, "Choose pics"), 665)
+        startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTO_CODE)
     }
 
 
